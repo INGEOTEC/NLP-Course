b32be9659b2121c7596403a5bedb2e917f8a828b
Mario Graff <mgraffg@ieee.org>
2021-05-20 11:18:31 -0500

    Machine Learning

--- code/points.py
+++ code/points.py
@@ -1,1 +1,13 @@
 from matplotlib import pylab as plt
+import numpy as np
+from sklearn import datasets
+plt.tick_params(axis="both", which="both", bottom=False,
+                top=False, left=False, right=False,
+                labelbottom=False, labelleft=False)
+
+plt.grid()
+X, _ = datasets.make_blobs(random_state=1, centers=3)
+plt.tight_layout()
+plt.plot(X[:, 0], X[:, 1], '.')
+plt.tight_layout()
+plt.savefig("points.png")
