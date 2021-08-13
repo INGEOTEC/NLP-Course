0824d58e09d834f9ee5b57a43148509575eb3b6d
Mario Graff <mgraffg@ieee.org>
2021-08-12 20:59:37 -0400

    hw1

--- code/hw1/hw1.py
+++ code/hw1/hw1.py
@@ -1,5 +1,6 @@
 import re
 from typing import Tuple
+from collections import Counter
 
 
 def start_project(txt: str) -> Tuple[int, int]:
@@ -13,5 +14,23 @@ def start_project(txt: str) -> Tuple[int, int]:
     >>> start_project("Hi! *** START OF THE PROJECT GUTENBERG EBOOK WAR AND PEACE *** Excellent book")
     (4, 77)
 
-    This function will be tested on two of the 25 most downloaded books from https://www.gutenberg.org. 
+    This function will be tested on two of the 
+    25 most downloaded books from https://www.gutenberg.org. 
     The format used is txt and the file is read using open(fname).read()"""
+
+
+def entity_identification(txt: str) -> Counter:
+    """Count the number of appearances of each entity using 
+    the simple heuristic of identifying as an entity 
+    each word starting with a capital letter or an abbreviation.
+    Usage:
+    
+    >>> entity_identification("America is a continent.")
+    Counter({'America': 1})
+    >>> entity_identification("The United States is in America")
+    Counter({'The': 1, 'United': 1, 'States': 1, 'America': 1})
+    >>> entity_identification("U.S.A. is in America and Mexico is in America")
+    Counter({'America': 2, 'U.S.A.': 1, 'Mexico': 1})
+    >>> entity_identification("USA. U.S.A America.")
+    Counter({'USA.': 1, 'U.S.A': 1, 'America.': 1})
+    """
