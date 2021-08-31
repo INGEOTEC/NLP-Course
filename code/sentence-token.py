c9b5a7742ad51cbb2a81c9ba57608f5eee274209
Mario Graff <mgraffg@ieee.org>
2021-08-31 10:03:42 -0400

    sentence

--- code/sentence-token.py
+++ code/sentence-token.py
@@ -1,5 +1,6 @@
 import nltk.data
 from microtc.utils import save_model
+from spacy.lang.en import English
 
 txt = """This eBook is for the use of anyone anywhere in the United States and
 most other parts of the world at no cost and with almost no restrictions
@@ -21,3 +22,23 @@ uns = open("hw1/tests/books/unsegmented.txt").read()
 
 output = sentence_detector.tokenize(uns)
 save_model(output, "sentence-tokenizer.pickle.gz")
+
+
+nlp = English()
+nlp.add_pipe("sentencizer")
+doc = nlp("This is a sentence. This is another sentence.")
+[x for x in doc.sents]
+
+_ = nlp("I am going to talk to M.G.G. because I want to")
+len([x for x in _.sents])
+
+[x for x in nlp(txt).sents]
+
+txt = """And he looked over at the alarm clock, ticking on the chest of drawers.
+“God in Heaven!” he thought. It was half past six and the hands were
+quietly moving forwards, it was even later than half past, more like
+quarter to seven. Had the alarm clock not rung? He could see from the
+bed that it had been set for four o’clock as it should have been; it
+certainly must have rung."""
+
+[x for x in nlp(txt).sents]
