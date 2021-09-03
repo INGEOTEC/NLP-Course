05c468482fab6a135096eb8334d3b524faa9cf5b
Mario Graff <mgraffg@ieee.org>
2021-09-03 16:46:43 -0400

    Tokenizer

--- code/tokenizer.py
+++ code/tokenizer.py
@@ -16,9 +16,10 @@ from microtc.params import OPTION_NONE
 from glob import glob
 from collections import Counter
 import numpy as np
-from scipy.optimize import minimize, minimize_scalar
+from scipy.optimize import minimize
 from matplotlib import pylab as plt
 from nltk.stem.porter import PorterStemmer
+from typing import Callable, Iterable
 
 
 tm = TextModel(num_option=OPTION_NONE, 
@@ -34,42 +35,71 @@ tm.tokenize("Hello good morning")
 
 # Count the number of words
 
-heaps = []
-counter = Counter()
-for fname in glob("books/*.txt"):
+def N_tokens_types(fname: str, 
+                   counter: Counter,
+                   tm: Callable[[str], Iterable[str]]):
     txt = open(fname).read()
-    tokens = tm.tokenize(txt)
-    counter.update(tokens)
-    V = len(counter)
+    tokens = tm(txt)
+    counter.update(tokens)    
     N = sum([v for v in counter.values()])
-    heaps.append([N, V])
+    return N, len(counter)
 
-plt.plot([x for x, _ in heaps], [x for _, x in heaps])
+
+counter = Counter()
+heaps = [N_tokens_types(fname, counter, tm.tokenize)
+         for fname in glob("books/*.txt")]
+
+
+plt.plot([x for x, _ in heaps], [x for _, x in heaps], '*')
 plt.grid()
 plt.xlabel("N")
 plt.ylabel("|V|")
 plt.tight_layout()
-plt.savefig("heaps-law.png", dpi=300) 
+plt.savefig("heaps-law2.png", dpi=300) 
 
-
-def error_heaps(beta):
+def error(coef):
     y = [y for _, y in heaps]
     x = np.array([x for x, _ in heaps])
-    hy = x**beta
+    hy = coef[0] * x**coef[1]
     return ((y - hy)**2).sum()
 
 
-res = minimize(error_heaps, [0.7], method='nelder-mead',
-               options={'xatol': 1e-8, 'disp': True})
+res = minimize(error, [1, 0.7], 
+               method='nelder-mead',
+               options={'disp': True})
 
-xx = minimize_scalar(error_heaps, bounds=(0, 1), method='bounded')               
 
-
-plt.plot([x for x, _ in heaps], [x for _, x in heaps])
+plt.plot([x for x, _ in heaps], [x for _, x in heaps], '.')
+plt.grid()
 x = np.array([x for x, _ in heaps])
-hy = x**res.x[0]
+hy = res.x[0] * x**res.x[1]
 plt.plot(x, hy)
+plt.xlabel("N")
+plt.ylabel("|V|")
+plt.tight_layout()
+plt.savefig("heaps-law3.png", dpi=300) 
 
 
 stemmer = PorterStemmer()
 stemmer.stem("playing")
+
+## Using another tokenizer
+
+tm = TextModel(num_option=OPTION_NONE, 
+               usr_option=OPTION_NONE,
+               url_option=OPTION_NONE, 
+               emo_option=OPTION_NONE, 
+               hashtag_option=OPTION_NONE,
+               ent_option=OPTION_NONE, 
+               lc=True, del_dup=False, del_punc=False,
+               del_diac=False, token_list=[-1])
+
+counter = Counter()
+heaps = [N_tokens_types(fname, counter, tm.tokenize)
+         for fname in glob("books/*.txt")]
+
+res = minimize(error, [1, 0.7], 
+               method='nelder-mead',
+               options={'disp': True})
+
+res.x
