cb29d839b27a2ba9e86f04c19745ae5efbcd9894
Mario Graff <mgraffg@ieee.org>
2021-09-09 17:56:25 -0400

    hw2

---
+++ code/hw2/hw2.py
@@ -0,0 +1,33 @@
+from spacy.pipeline.sentencizer import Sentencizer
+
+
+class ColgateSBD(Sentencizer):
+    def predict(self, docs):    
+        """Apply the pipe to a batch of docs, without modifying them.
+
+        docs (Iterable[Doc]): The documents to predict.
+        RETURNS: The predictions for each document.
+        """
+        if not any(len(doc) for doc in docs):
+            # Handle cases where there are no tokens in any docs.
+            guesses = [[] for doc in docs]
+            return guesses
+        guesses = []
+        for doc in docs:
+            doc_guesses = [False] * len(doc)
+            if len(doc) > 0:
+                start = 0
+                seen_period = False
+                doc_guesses[0] = True
+                for i, token in enumerate(doc):
+                    is_in_punct_chars = token.text in self.punct_chars
+                    if seen_period and not token.is_punct and not is_in_punct_chars:
+                        doc_guesses[start] = True
+                        start = token.i
+                        seen_period = False
+                    elif is_in_punct_chars:
+                        seen_period = True
+                if start < len(doc):
+                    doc_guesses[start] = True
+            guesses.append(doc_guesses)
+        return guesses  
---
+++ code/hw2/performance.py
@@ -0,0 +1,47 @@
+import numpy as np
+from spacy.pipeline.sentencizer import Sentencizer
+from glob import glob
+from spacy.lang.en import English
+
+
+def metrics(a, b):
+    from sklearn.metrics import f1_score, recall_score, precision_score, accuracy_score
+    return (accuracy_score(a, b),
+            recall_score(a, b),
+            precision_score(a, b),
+            f1_score(a, b))
+
+
+def performance(colgate=None):
+    colgate = colgate if colgate is not None else Sentencizer()
+    nlp = English()
+    colgate = ColgateSBD()
+
+    output = []
+    for test in glob("marked-*.txt"):
+        input = test.replace("marked-", "") 
+        txt = open(input).read()
+        tokens = nlp(open(test).read())
+        hy_tokens = colgate(nlp(txt))
+        assert len(tokens) == len(hy_tokens)
+        _ = [b.is_punct and a.text == "#"
+             for a, b in zip(tokens, hy_tokens)]
+        y = np.empty_like(_, dtype=bool)
+        y[1:] = _[:-1]
+        y[0] = True
+        hy = np.array([x.is_sent_start for x in hy_tokens])
+        _ = metrics(y, hy)
+        output.append((test, _, y.sum()))
+    return output
+
+
+if __name__ == "__main__":
+    from hw2 import ColgateSBD
+    from glob import glob
+    from spacy.lang.en import English
+
+    output = performance(ColgateSBD())
+    for input, perf, n_sent in output:
+        print("Input:", input, perf, "Number of sentences:", n_sent)
+
+
