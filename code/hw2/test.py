69dec95b8dcef8a9b9cde3f5001f3d583551746a
Mario Graff <mgraffg@ieee.org>
2021-09-22 13:31:52 -0400

    Performance

--- code/hw2/test.py
+++ code/hw2/test.py
@@ -1,24 +1,26 @@
 import json
+import numpy as np
 
 if __name__ == '__main__':
     from performance import performance
     from hw2 import ColgateSBD
     perf = performance(ColgateSBD())
+    perf2 = performance()
 
     num_sent = sum([x[-1] for x in perf])
     tests = [dict(name="Number of sentences",
                   max_score=1,
                   score=int(num_sent))]
-    for fname, *per in perf:
-        for name, value in zip(["Accuracy", "Recall", "Precision", "F1"],
-                                per[0]):
+    for (fname, *per), (fname, *per2) in zip(perf, perf2):
+        for name, value, value2 in zip(["Accuracy", "Recall", "Precision", "F1"],
+                                per[0], per2[0]):
             _ = dict(name=fname + ": " + name,
                      max_score=1,
-                     score=float(value))
+                     score=float(np.fabs(value - value2)))
             tests.append(_)
     output = dict(visibility="visible",
                   tests=tests)
     _ = '/autograder/results/results.json'
-    # _ = 'results.json'
+    _ = 'results.json'
     with open(_, 'w') as f:
         json.dump(output, f)
