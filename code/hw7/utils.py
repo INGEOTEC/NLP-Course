8487f7eba2bcb69f4c49dff49a30fbfcba78786e
Mario Graff <mgraffg@ieee.org>
2021-11-30 16:42:26 -0500

    hw7

---
+++ code/hw7/utils.py
@@ -0,0 +1,36 @@
+from EvoMSA.utils import download
+from microtc.utils import load_model
+from scipy.sparse import hstack
+
+
+def dataset(lst):
+    output = []
+    for x in lst:
+        _ = dict(text=[x['sentence1'], x['sentence2']],
+                 sentence1=x['sentence1'],
+                 sentence2=x['sentence2'],
+                 klass=x['klass'])
+        output.append(_)
+    return output
+
+
+def transform(data):
+    tm = load_model(download('b4msa_En.tm'))
+    i, X = data
+    return i, tm.transform(X)
+
+
+def transform2(data):
+    tm = load_model(download('b4msa_En.tm'))
+    i, X = data
+    sent1 = tm.transform([x['sentence1'] for x in X])
+    sent2 = tm.transform([x['sentence2'] for x in X])
+    return i, hstack((sent1, sent2))
+
+
+def transform3(data):
+    tm = load_model(download('b4msa_En.tm'))
+    i, X = data
+    sent1 = tm.transform([x['sentence1'] for x in X])
+    sent2 = tm.transform([x['sentence2'] for x in X])
+    return i, sent1 - sent2
