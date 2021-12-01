923093b748d2fc85f7cdb3cc3919879c43543995
Mario Graff <mgraffg@ieee.org>
2021-11-30 21:40:17 -0500

    hw7 - gradescope

--- code/hw7/hw7.py
+++ code/hw7/hw7.py
@@ -1,11 +1,9 @@
 import json
 from microtc.utils import tweet_iterator
 from sklearn.svm import LinearSVC
-from EvoMSA.utils import bootstrap_confidence_interval
 import numpy as np
 from multiprocessing import Pool, cpu_count
 from tqdm import tqdm
-from utils import dataset
 from utils import transform as transform
 from scipy.sparse import vstack
 
@@ -13,8 +11,6 @@ from scipy.sparse import vstack
 if __name__ == '__main__':
     train = list(tweet_iterator('snli_train.json'))
     test = list(tweet_iterator('snli_test.json'))
-    # train = dataset(train)
-    # test = dataset(test)
     with Pool(cpu_count() - 1) as pool:
         _ = [(i, train[i:i+10000]) for i in range(0, len(train), 10000)]
         Xt = [i for i in tqdm(pool.imap_unordered(transform, _), total=len(_))]
--- code/hw7/utils.py
+++ code/hw7/utils.py
@@ -3,27 +3,16 @@ from microtc.utils import load_model
 from scipy.sparse import hstack
 
 
-def dataset(lst):
-    output = []
-    for x in lst:
-        _ = dict(text=[x['sentence1'], x['sentence2']],
-                 sentence1=x['sentence1'],
-                 sentence2=x['sentence2'],
-                 klass=x['klass'])
-        output.append(_)
-    return output
-
-
 def transform(data):
-    tm = load_model(download('b4msa_En.tm'))
     i, X = data
+    tm = load_model(download('b4msa_En.tm'))
     concat = [[x['sentence1'], x['sentence2']] for x in X]
     return i, tm.transform(concat)
 
 
 def transform2(data):
-    tm = load_model(download('b4msa_En.tm'))
     i, X = data
+    tm = load_model(download('b4msa_En.tm'))
     sent1 = tm.transform([x['sentence1'] for x in X])
     sent2 = tm.transform([x['sentence2'] for x in X])
     return i, hstack((sent1, sent2))
