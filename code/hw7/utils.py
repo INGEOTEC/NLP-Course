3466cfd758ab422fe7ea65815b9b6ac5d7843424
Mario Graff <mgraffg@ieee.org>
2021-12-01 11:37:54 -0500

    hw7

--- code/hw7/utils.py
+++ code/hw7/utils.py
@@ -4,17 +4,13 @@ from scipy.sparse import hstack
 
 
 def transform(data):
-    # i, X = data
-    X = data
     tm = load_model(download('b4msa_En.tm'))
-    concat = [[x['sentence1'], x['sentence2']] for x in X]
+    concat = [[x['sentence1'], x['sentence2']] for x in data]
     return tm.transform(concat)
 
 
 def transform2(data):
-    # i, X = data
-    X = data
     tm = load_model(download('b4msa_En.tm'))
-    sent1 = tm.transform([x['sentence1'] for x in X])
-    sent2 = tm.transform([x['sentence2'] for x in X])
-    return hstack((sent1, sent2))
+    sent1 = tm.transform([x['sentence1'] for x in data])
+    sent2 = tm.transform([x['sentence2'] for x in data])
+    return hstack((sent1, sent2))  
