633234a470e033ae29421ba9c2aaf56494f1fba9
Mario Graff <mgraffg@ieee.org>
2022-02-22 11:31:29 -0500

    hw

--- code/hw3/gradescope/test.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from gradescope_utils.autograder_utils.json_test_runner import JSONTestRunner
-
-if __name__ == '__main__':
-    suite = unittest.defaultTestLoader.discover('tests')
-    output = '/autograder/results/results.json'
-    # output = 't.json'
-    with open(output, 'w') as f:
-        JSONTestRunner(visibility='visible', stream=f).run(suite)
